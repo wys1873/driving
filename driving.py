ce542cfa0c08f6b8605759c4cd6a3356572c3158
yswang <977719395@qq.com>
2021-07-08 21:15:55 +0800

    add country

---
+++ driving.py
@@ -0,0 +1,15 @@
+age= input('请输入年龄：')
+age=int(age)
+country=input('请输入国家：')
+if country=='中国':
+	if age>=18:
+		print('可以考驾照')
+	else:
+		print('不可以')
+elif country=='美国':
+	if age>=16:
+		print('可以考驾照')
+	else:
+		print('不可以')
+
+
